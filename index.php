324107442eeb15e034d439d3b26d415d1cbf855e
jhartne1 <jhartne1@CompSciICv-49.fsc.int>
2025-03-31 10:49:30 -0400

    username fix 4

--- index.php
+++ index.php
@@ -12,30 +12,27 @@
 <body style="height:1500px">
 
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-  <a class="navbar-brand" href="index.php">Attendify</a>
-  <ul class="navbar-nav">
-    <?php
-    session_start();
-    if (isset($_SESSION['username'])) {
-        // Show welcome page link if logged in
-        if ($_SESSION['role'] == 'attendee') {
-            echo '<li class="nav-item">
-                    <a class="nav-link" href="welcome_attendee.php">Dashboard</a>
-                  </li>';
-        }
-        echo '<li class="nav-item">
+    <a class="navbar-brand" href="index.php">Attendify</a>
+    <ul class="navbar-nav">
+        <?php session_start(); ?>
+        <?php if (isset($_SESSION['username'])): ?>
+            <li class="nav-item">
+                <a class="nav-link" href="welcome_<?php echo $_SESSION['role']; ?>.php">
+                    Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>
+                </a>
+            </li>
+            <li class="nav-item">
                 <a class="nav-link" href="logout.php">Logout</a>
-              </li>';
-    } else {
-    ?>
-      <li class="nav-item">
-        <a class="nav-link" href="registration.php">Register</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="LogInPage.php">Log In</a>
-      </li>
-    <?php } ?>
-  </ul>
+            </li>
+        <?php else: ?>
+            <li class="nav-item">
+                <a class="nav-link" href="registration.php">Register</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Log In</a>
+            </li>
+        <?php endif; ?>
+    </ul>
 </nav>
 
 
